e0f0367c4a4c395288b5de5974f30c252d30e9b3
Abraham Huerta <abrahamhuertadev@outlook.es>
2025-02-14 11:09:25 -0600

    Release

--- src/screens/AccountDetailScreen.tsx
+++ src/screens/AccountDetailScreen.tsx
@@ -10,22 +10,25 @@ import {
   TouchableWithoutFeedback,
   ActivityIndicator,
   Clipboard,
-  Keyboard
+  Keyboard,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {getAccountById, deleteAccount} from '../database/db';
 import {useRoute, useNavigation, RouteProp} from '@react-navigation/native';
-import {TOTP } from 'totp-generator'; // Importamos la librería totp-generator
+import {TOTP} from 'totp-generator'; // Importamos la librería totp-generator
 import Toast from 'react-native-toast-message'; // Impor
-import { RootStackParamList } from '../../App'; // Importamos los tipos
-import { StackNavigationProp } from '@react-navigation/stack';
+import {RootStackParamList} from '../../App'; // Importamos los tipos
+import {StackNavigationProp} from '@react-navigation/stack';
 import {useFocusEffect} from '@react-navigation/native'; // Hook para actualizar al enfocar la pantalla
 type AccountDetailScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
   'AccountDetail'
 >;
 
-type AccountDetailScreenRouteProp = RouteProp<RootStackParamList, 'AccountDetail'>;
+type AccountDetailScreenRouteProp = RouteProp<
+  RootStackParamList,
+  'AccountDetail'
+>;
 function AccountDetailScreen() {
   const navigation = useNavigation<AccountDetailScreenNavigationProp>();
   const route = useRoute<AccountDetailScreenRouteProp>();
@@ -47,7 +50,7 @@ function AccountDetailScreen() {
   useFocusEffect(
     React.useCallback(() => {
       fetchAccountDetails();
-    }, [])
+    }, []),
   );
 
   const confirmDeleteAccount = async () => {
@@ -106,21 +109,21 @@ function AccountDetailScreen() {
   const generateOtp = (secret: string) => {
     // timestamp obten el tiempoa ctual en hora America/Mexico_City
     let timestamp = getMexicoTimestamp();
-    const { otp, expires } = TOTP.generate(secret, {
-      digits: 6
-    })
+    const {otp, expires} = TOTP.generate(secret, {
+      digits: 6,
+    });
     const date = new Date(expires);
-const humanReadableDate = date.toLocaleString(); // Converts to a human-readable format
+    const humanReadableDate = date.toLocaleString(); // Converts to a human-readable format
 
-console.log(humanReadableDate, otp, timestamp, expires);
+    console.log(humanReadableDate, otp, timestamp, expires);
     setTotpCode(otp);
   };
 
   const startCountdown = () => {
     intervalRef.current = setInterval(() => {
-      setExpiresIn((prev) => {
+      setExpiresIn(prev => {
         if (prev === 1) {
-            console.log('Regenerando OTP', accountData);
+          console.log('Regenerando OTP', accountData);
           generateOtp(accountData.secret); // Regenerar OTP cada 30 segundos
           return 30;
         }
@@ -157,14 +160,25 @@ console.log(humanReadableDate, otp, timestamp, expires);
         </TouchableOpacity>
         <Text style={styles.headerTitle}>Detalles</Text>
         <View style={styles.headerActions}>
-        <TouchableOpacity
-            onPress={() => navigation.navigate('EditAccount', {accountId: accountId})} 
-          >
-          <Icon name="pencil" size={24} color="#65A518" style={styles.headerIcon}   />
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate('EditAccount', {accountId: accountId})
+            }>
+            <Icon
+              name="pencil"
+              size={24}
+              color="#65A518"
+              style={styles.headerIcon}
+            />
           </TouchableOpacity>
           <TouchableOpacity onPress={handleDeleteAccount}>
-  <Icon name="trash" size={24} color="#65A518" style={styles.headerIcon} />
-</TouchableOpacity>
+            <Icon
+              name="trash"
+              size={24}
+              color="#65A518"
+              style={styles.headerIcon}
+            />
+          </TouchableOpacity>
         </View>
       </View>
 
@@ -172,7 +186,9 @@ console.log(humanReadableDate, otp, timestamp, expires);
         <Text style={styles.accountName}>{account.name}</Text>
         <Text style={styles.accountEmail}>{account.email}</Text>
 
-        <Text style={styles.descriptionText}>Esta es tu contraseña de un solo uso:</Text>
+        <Text style={styles.descriptionText}>
+          Esta es tu contraseña de un solo uso:
+        </Text>
 
         <Text style={styles.totpCode}>{totpCode}</Text>
 
@@ -181,21 +197,25 @@ console.log(humanReadableDate, otp, timestamp, expires);
         </TouchableOpacity>
 
         <View style={styles.expirationContainer}>
-        <View style={styles.circle}>
-          <Text style={styles.expirationText}>Expira en</Text>
-          <Text style={styles.expirationTime}>{expiresIn}</Text>
-          <Text style={styles.expirationText}>segundos</Text>
+          <View style={styles.circle}>
+            <Text style={styles.expirationText}>Expira en</Text>
+            <Text style={styles.expirationTime}>{expiresIn}</Text>
+            <Text style={styles.expirationText}>segundos</Text>
+          </View>
         </View>
-      </View>
       </View>
       <Modal visible={isModalVisible} transparent animationType="fade">
         <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
           <View style={styles.modalOverlay}>
             <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
               <View style={styles.modalContainer}>
-                <Text style={styles.modalTitle}>Confirma que deseas eliminar la cuenta</Text>
+                <Text style={styles.modalTitle}>
+                  Confirma que deseas eliminar la cuenta
+                </Text>
                 <Text style={styles.accountNameModal}>{account?.name}</Text>
-                <TouchableOpacity style={styles.deleteButton} onPress={confirmDeleteAccount}>
+                <TouchableOpacity
+                  style={styles.deleteButton}
+                  onPress={confirmDeleteAccount}>
                   <Text style={styles.deleteButtonText}>Eliminar cuenta</Text>
                 </TouchableOpacity>
               </View>
@@ -275,7 +295,7 @@ const styles = StyleSheet.create({
   },
   circle: {
     width: 200, // Tamaño más grande para el círculo
-    height: 200, 
+    height: 200,
     borderRadius: 100, // Radio ajustado para mantener la forma circular
     borderWidth: 3, // Borde más grueso
     borderColor: '#65A518',
@@ -334,7 +354,5 @@ const styles = StyleSheet.create({
     color: '#293845',
   },
 });
-
-
 
 export default AccountDetailScreen;
